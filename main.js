07ebb309784dcbd6ea3b7fbcd0679af21bfc51f4
YURI TRIQUES DOS SANTOS <106687697+yuri07br@users.noreply.github.com>
2024-06-20 08:30:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixaperguntas = document.querySelector('.caixa-perguntas');
+const caixaalternativas = document.querySelector('.caixa-alternativa');
+const caixaresultado = document.querySelector('.caixa-resultado');
+const textoresultado = document.querySelector('.texto-resultado');
+const perguntas = [
+    {
+        enunciado:"pergunta 01",
+        alternativa:[
+            "alternativa01",
+            "alternativa02"
+        ]
+    },
+    {
+        enunciado:"pergunta 02",
+        alternativa:[
+            "alternativa01",
+            "alternativa02"
+        ]
+    },
+    {
+        enunciado:"pergunta 03",
+        alternativa:[
+            "alternativa01",
+            "alternativa02"
+        ]
+    },
+    {
+        enunciado:"pergunta 04",
+        alternativa:[
+            "alternativa01",
+            "alternativa02"
+        ]
+    },
+    {
+        enunciado:"pergunta 05",
+        alternativa:[
+            "alternativa01",
+            "alternativa02"
+        ]
+    },
+]
